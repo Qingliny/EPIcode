7a0a6f614ea9328115b854f446b8ed221f09f9f7
Qinglin <qy2211@columbia.edu>
2020-01-03 18:06:31 -0800

    update

---
+++ PrimitiveType/count_bits.py
@@ -0,0 +1,17 @@
+from test_framework import generic_test
+
+
+def count_bits(x):
+    # TODO - you fill in here.
+    num = 0
+    while x:
+        if x & 1:
+            num += 1
+        x >> 1
+    return num
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main("count_bits.py", 'count_bits.tsv',
+                                       count_bits))
